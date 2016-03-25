846e88b77c6e2bbe0cfe970f4f8de84ad047a935
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-03-25 19:16:05 +0000

    web sockets?

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var ws = require("nodejs-websocket")
 
 var app = express();
 require('dotenv').load();
@@ -17,7 +18,22 @@ app.use(session({
 }));
 
 
+
+// Scream server example: "hi" -> "HI!!!"
+var server = ws.createServer(function (conn) {
+    console.log("New connection")
+    conn.on("text", function (str) {
+        console.log("Received "+str)
+        conn.sendText(str.toUpperCase()+"!!!")
+    })
+    conn.on("close", function (code, reason) {
+        console.log("Connection closed")
+    })
+}).listen(8001)
+
 routes(app);
+
+
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
