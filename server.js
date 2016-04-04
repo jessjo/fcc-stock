7393734ae57422c26ffe9025124a719837141f69
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-04-04 22:24:05 +0000

    method for listening on page

--- server.js
+++ server.js
@@ -31,10 +31,31 @@ wsServer.on('request', function(r){
     
     // Specific id for this client & increment count
     var id = count++;
-    
+
     // Store the connection method so we can loop through & contact all clients
     clients[id] = connection;
+    console.log((new Date()) + ' Connection accepted [' + id + ']');
+    
+    connection.on('message', function(message) {
+
+        // The string message that was sent to us
+        var msgString = message.utf8Data;
+
+        // Loop through all clients
+        for(var i in clients){
+            // Send a message to the client with the message
+            clients[i].sendUTF(msgString);
+        }
+
+    });
+
+    connection.on('close', function(reasonCode, description) {
+        delete clients[id];
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
 });
+
+
 
 mongoose.connect(process.env.MONGO_URI);
 
