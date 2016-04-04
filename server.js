c0d21bc1f5c385b0969f32ce5b017001394a6d05
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-04-04 23:32:46 +0000

    working on wss

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ var express = require('express');
 var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var session = require('express-session');
-var ws = require("nodejs-websocket")
+var ws = require("nodejs-websocket");
+var fs = require("fs");
+var https = require ("https");
 
 
 var app = express();
 require('dotenv').load();
 var http = require('http');
+var privateKey  = fs.readFileSync('sslcert/key.pem', 'utf8');
+var certificate = fs.readFileSync('sslcert/cert.pem', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
 var server = http.createServer(function(request, response) {});
 
 server.listen(1234, function() {
@@ -75,3 +80,6 @@ var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
+
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8443);
