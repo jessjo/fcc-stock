428d70b8245179aa0083e6e865a3de519d280376
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-04-15 20:49:06 +0000

    web sockets attempts...

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var express = require('express');
 var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var session = require('express-session');
-var ws = require("nodejs-websocket");
+var WebSocketServer = require('websocket').server;
+
 var fs = require("fs");
 var https = require ("https");
-
 
 var app = express();
 require('dotenv').load();
@@ -16,52 +16,63 @@ var privateKey  = fs.readFileSync('key.pem', 'utf8');
 var certificate = fs.readFileSync('cert.pem', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 
-
+/**
 var server = https.createServer(credentials, app);
 
 server.listen(1234, function() {
     console.log((new Date()) + ' Server is listening on port 1234');
 });
+**/
 
-var WebSocketServer = require('websocket').server;
-var wsServer = new WebSocketServer({
-    httpServer: server
+var server = https.createServer(credentials, app, function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
 });
 
-var count = 0;
-var clients = [];
-
-wsServer.on('request', function(r){
-    // Code here to run on connection
+server.listen(1234, function() {
+    console.log((new Date()) + ' Server is listening on port 1234');
+});
+ 
+var wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production 
+    // applications, as it defeats all standard cross-origin protection 
+    // facilities built into the protocol and the browser.  You should 
+    // *always* verify the connection's origin and decide whether or not 
+    // to accept it. 
+    autoAcceptConnections: false
+});
+ 
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed. 
+  return true;
+}
+ 
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin 
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
     
-    var connection = r.accept('echo-protocol', r.origin);
-    
-    // Specific id for this client & increment count
-    var id = count++;
-
-    // Store the connection method so we can loop through & contact all clients
-    clients[id] = connection;
-    console.log((new Date()) + ' Connection accepted [' + id + ']');
-    
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message) {
-
-        // The string message that was sent to us
-        var msgString = message.utf8Data;
-
-        // Loop through all clients
-        for(var i in clients){
-            // Send a message to the client with the message
-            clients[i].sendUTF(msgString);
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
         }
-
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
     });
-
     connection.on('close', function(reasonCode, description) {
-        delete clients[id];
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
-
 
 
 mongoose.connect(process.env.MONGO_URI);
