4d326c13c5f4c4ea8156f3d5f4e3721937c86dd1
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-04-20 16:32:03 +0000

    another tweak

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ var express = require('express');
 var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var session = require('express-session');
-var WebSocketServer = require('websocket').server;
 
 var fs = require("fs");
 var https = require ("https");
 var app = express()();
-var io = require('socket.io').listen(app);
 
-var http = require('http').Server(app);
+var http = require('http');
 require('dotenv').load();
 
 mongoose.connect(process.env.MONGO_URI);
@@ -26,31 +24,6 @@ app.use(session({
 http.listen(1234, function(){
   console.log('http listening on *:1234');
 });
-
-io.sockets.on('connection', function (socket) {
-    console.log('connection');
-    socket.on('setPseudo', function (data) {
-        socket.set('pseudo', data);
-    });
-    
-    socket.on('message', function (message) {
-    socket.get('pseudo', function (error, name) {
-        var data = { 'message' : message, pseudo : name };
-        socket.broadcast.emit('message', data);
-        console.log("user " + name + " send this : " + message);
-    })
-});
-});
-
-/**
-
-var privateKey  = fs.readFileSync('key.pem', 'utf8');
-var certificate = fs.readFileSync('cert.pem', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
-
-var httpServer = https.createServer(credentials, app);
-
-**/
 
 
 
