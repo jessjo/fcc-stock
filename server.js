c4bb3f2f46d6acc7081b7a4becbafcb735aff78c
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-04-20 20:53:39 +0000

    connections working

--- server.js
+++ server.js
@@ -4,52 +4,26 @@ var express = require('express');
 var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var session = require('express-session');
-var socketio = require('socket.io');
 var fs = require("fs");
 var https = require ("https");
 var app = express();
-var http = require('http');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 require('dotenv').load();
 var async = require('async');
 
 mongoose.connect(process.env.MONGO_URI);
 
-var server = http.createServer(app);
-var io = socketio.listen(server);
+
 
 
 //socket.io
 var messages = [];
 var sockets = [];
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-        var data = {
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      
-    });
-
-   
-  });
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 
 
@@ -66,8 +40,7 @@ routes(app);
 
 
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+http.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function(){
+  console.log("Chat server listening at 8080");
 });
 
