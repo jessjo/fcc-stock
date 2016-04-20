b58553c01e941ee606f27a245ae602d84ddb1b70
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-04-20 21:02:07 +0000

    socket.io tweaks

--- server.js
+++ server.js
@@ -23,7 +23,16 @@ var sockets = [];
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
+
 
 
 
